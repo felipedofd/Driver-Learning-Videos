4ef51289f657a328b413c6b14ad0ad3fece44045
Felipe Anzolin <75090553+felipedofd@users.noreply.github.com>
2022-09-10 21:06:50 -0300

    Create SplashActivity.kt

---
+++ app/src/main/java/com/example/flowpodcast/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.flowpodcast
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.flowpodcast.databinding.ActivitySplashBinding
+
+private lateinit var binding: ActivitySplashBinding
+
+lateinit var handler: Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
