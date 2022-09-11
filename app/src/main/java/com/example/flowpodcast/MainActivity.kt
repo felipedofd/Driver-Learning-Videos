e7ac23d3b5d5e26e91bd40fee5085d90a5583c37
Felipe Anzolin <75090553+felipedofd@users.noreply.github.com>
2022-09-10 22:52:11 -0300

    implement linkedin link and change splashscreen images

--- app/src/main/java/com/example/flowpodcast/MainActivity.kt
+++ app/src/main/java/com/example/flowpodcast/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.flowpodcast.controller.ListEpisodesAdapter
 import com.example.flowpodcast.databinding.ActivityMainBinding
 import com.example.flowpodcast.player.PlayerActivity
+import com.example.flowpodcast.view.MyLinkedinPageActivity
 import com.example.flowpodcast.viewModel.FlowViewModel
 import com.example.flowpodcast.view.WebViewActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -34,6 +35,12 @@ class MainActivity : AppCompatActivity() {
         binding.flowPage.setOnClickListener {
             val intent = Intent(this, WebViewActivity::class.java)
             intent.putExtra("webiew", false)
+            startActivity(intent)
+        }
+
+        binding.myLinkedinPage.setOnClickListener {
+            val intent = Intent(this, MyLinkedinPageActivity::class.java)
+            intent.putExtra("my_linkedin_perfil", false)
             startActivity(intent)
         }
 
